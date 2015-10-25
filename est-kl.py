a76d91070c95268e99e61cf87deb6945e089b782
Jorg Bornschein <jb@capsec.org>
2015-10-24 21:31:43 -0400

    Update est-kl.py

--- est-kl.py
+++ est-kl.py
@@ -64,6 +64,7 @@ if __name__ == "__main__":
     #----------------------------------------------------------------------
     logger.info("Compiling function...")
 
+    n_layers = len(brick.p_layers)
     n_samples = tensor.iscalar('n_samples')
     x = tensor.matrix('features')
     batch_size = x.shape[0]
@@ -88,45 +89,46 @@ if __name__ == "__main__":
     log_qp = (log_q_all-log_p_all)
     log_kl = tensor.sum(log_qp, axis=1) / n_samples
 
-    kl = log_kl + log_px
+    total_kl = log_kl + log_px
+    layer_kl = [tensor.sum(lq-lp, axis=1) / n_samples for lp, lq in zip(log_p[:], log_q[:])]
 
     do_kl = theano.function(
                         [x, n_samples], 
-                        [log_px, kl],
+                        [log_px, total_kl]+layer_kl,
                         name="do_kl", allow_input_downcast=True)
 
     #----------------------------------------------------------------------
     logger.info("Loading dataset...")
 
     n_samples = args.nsamples
-    batch_size = 5*max(1, 100000 // args.nsamples)
+    batch_size = 5*max(1, 10000 // args.nsamples)
     
     x_dim, stream_train, stream_valid, stream_test = datasets.get_streams(args.data, batch_size)
-
     stream = stream_test
 
     log_px = np.array([])
-    kl = np.array([])
+    total_kl = np.array([])
+    layer_kl = [np.array([]) for _ in xrange(n_layers)]
     for batch in stream.get_epoch_iterator():
         features = batch[0]
-        log_px_batch, kl_batch = do_kl(features, n_samples)
+        ret = do_kl(features, n_samples)
+        log_px_batch, total_kl_batch = ret[:2]
+        layer_kl_batch = ret[2:]
 
         log_px = np.concatenate([log_px, log_px_batch])
-        kl = np.concatenate([kl, kl_batch])
-        #print(plog_px_batch)
-        #print(kl_batch)
+        total_kl = np.concatenate([total_kl, total_kl_batch])
+        for l, kl_batch in enumerate(layer_kl_batch):
+            layer_kl[l] = np.concatenate([layer_kl[l], kl_batch])
 
-        #features = data_train.get_data(None, n)[0]
-        #features = features.reshape((1, x_dim))
-        #ep, eps = do_ess(features, n_samples)
-        #ess_p.append(ep)
-        #ess_ps.append(eps)
-
-    print(kl.shape)
+    print(total_kl.shape)
     print(log_px.shape)
 
-    print("KL(q|p) : %f +-%f (std: %f)" % (kl.mean(), stats.sem(kl), np.std(kl)))
     print("log p(x): %f +-%f (std: %f)" % (log_px.mean(), stats.sem(log_px), np.std(log_px)))
+    print("KL(q|p) : %f +-%f (std: %f)" % (total_kl.mean(), stats.sem(total_kl), np.std(total_kl)))
+    print()
+    for l, kl in enumerate(layer_kl):
+        print("KL%d(q|p) : %f +-%f (std: %f)" % (l, kl.mean(), stats.sem(kl), np.std(kl)))
+        
 
     #print("ESS p : %f +-%f (std: %f)" % (ess_p.mean(), stats.sem(ess_p), np.std(ess_p)))
     #print("ESS p*: %f +-%f (std: %f)" % (ess_ps.mean(), stats.sem(ess_ps), np.std(ess_ps)))
